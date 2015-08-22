7d295e041586a6ea33e15ddc53b890ecb985a5b6
Gavin <gmeredith15855@gmail.com>
2015-08-22 11:45:18 +0930

    added functionality for reading and loading maps into the game

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import glob
 
 class Game:
 	"""Main game logic"""
@@ -8,14 +9,29 @@ class Game:
 
 		infoObject = pygame.display.Info()
 
+		#Get user screen width and height
 		self.width = infoObject.current_w
 		self.height = infoObject.current_h
 
-		self.screen = pygame.display.set_mode((width, height))
+		#Load all maps into a map array
+		self.maps = glob.glob('./maps/*.txt')
+
+		#Set screen to users screen size
+		self.screen = pygame.display.set_mode((800, 600))
 		pygame.display.set_caption('Hue')
 
 		self.running = True
 		self.framerate = 20
+
+	def loadMap(self, fileName):
+		try:
+			#Open map file
+			f = open(fileName, 'r')
+			for line in f:
+				print line
+			pass
+		except Exception, e:
+			raise e
 
 	def main(self):
 		while self.running:
@@ -27,11 +43,6 @@ class Game:
 		pygame.display.flip()
 
 		self.clock.tick(self.framerate)
-
-	def readLevels(self, fileName):
-		f = open(fileName, 'r')
-		for line in f:
-			print line
 
 
 window = Game()
