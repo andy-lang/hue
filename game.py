aae2e510c260da0f391635d6494003ee85e89049
andy-lang <langy26@gmail.com>
2015-08-22 11:04:29 +0930

    Add main game loop and window

---
+++ game.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Game:
+	"""Main game logic"""
+
+	def __init__(self, width = 800, height = 600):
+		self.width = width
+		self.height = height
+		self.screen = pygame.display.set_mode((width,height))
+
+		self.running = True
+		self.framerate = 20
+
+	def main(self):
+		while self.running:
+			for event in pygame.event.get():
+				# events go here
+				if event.type == pygame.QUIT:
+					self.running = False
+
+		pygame.display.flip()
+
+		self.clock.tick(self.framerate)
+	
+	
+window = Game()
+window.main()
