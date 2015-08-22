5652d292354d73b84fd1a4226280b41c3173a1b5
andy-lang <langy26@gmail.com>
2015-08-22 11:09:38 +0930

    Add window title

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ class Game:
 	def __init__(self, width = 800, height = 600):
 		self.width = width
 		self.height = height
+
 		self.screen = pygame.display.set_mode((width,height))
+		pygame.display.set_caption('Hue')
 
 		self.running = True
 		self.framerate = 20
